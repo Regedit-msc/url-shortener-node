02318e94a800da0b8f096e9d449f21095ab81a22
Regedit-msc <efusanyaae@gmail.com>
2020-12-08 11:22:35 +0100

    added procfile

---
+++ models/url.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const shortId = require("shortid");
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+
+    // Use shortid package to generate random short ids
+    default: shortId.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema);
